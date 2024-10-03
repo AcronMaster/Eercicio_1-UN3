6c8faf780459a221591f490c0698c8b28cadbd2d
Luis Adrián Cauich Dzul <cauichadrian04@gmail.com>
2024-10-02 20:01:17 -0600

    Add files via upload

---
+++ pila_ejemplo1.py
@@ -0,0 +1,17 @@
+# Crear una pila vacía
+pila = []
+
+# Apilar elementos (agregar al final de la lista)
+pila.append(1)
+print("Apilado:", 3)
+pila.append(2)
+print("Apilado:", 4)
+pila.append(3)
+print("Apilado:", 5)
+
+# Mostrar el estado de la pila
+print("Pila actual:", pila)
+
+
+
+
